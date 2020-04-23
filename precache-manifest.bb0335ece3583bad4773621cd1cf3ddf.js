ad8cbfb76ed44a75c5aaa5fbe5ec434a640b8096
Ryan Siu <ryansiu17@gmail.com>
2020-04-23 07:46:48 -0700

    Updates

---
+++ precache-manifest.bb0335ece3583bad4773621cd1cf3ddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94d5193e968d2c1acddd66732b17f6f",
+    "url": "/relearn/index.html"
+  },
+  {
+    "revision": "8ff557553136ef39110b",
+    "url": "/relearn/static/css/main.de67f5dd.chunk.css"
+  },
+  {
+    "revision": "649d000e5dc25df52fdb",
+    "url": "/relearn/static/js/2.cd4e19af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/relearn/static/js/2.cd4e19af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff557553136ef39110b",
+    "url": "/relearn/static/js/main.6e2d76e3.chunk.js"
+  },
+  {
+    "revision": "a97634bc89d73d647f93",
+    "url": "/relearn/static/js/runtime-main.44704970.js"
+  },
+  {
+    "revision": "6bdd124459a8bbafb7fee2ebe79ec142",
+    "url": "/relearn/static/media/m.6bdd1244.webp"
+  }
+]);
